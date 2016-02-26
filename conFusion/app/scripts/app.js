7da16cc6de6b076859255acfa152bf2e776fa582
Green88 <taniagr0808@gmail.com>
2016-02-26 18:19:07 +0200

    second assignment  - scope and forms

--- conFusion/app/scripts/app.js
+++ conFusion/app/scripts/app.js
@@ -94,4 +94,76 @@ angular.module('confusionApp', [])
                 console.log($scope.feedback);
             }
         };
+    }])
+    .controller('DishDetailController', ['$scope', function($scope) {
+
+        $scope.dish={
+            name:'Uthapizza',
+            image: 'images/uthapizza.png',
+            category: 'mains',
+            label:'Hot',
+            price:'4.99',
+            description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+            comments: [
+                {
+                    rating:5,
+                    comment:"Imagine all the eatables, living in conFusion!",
+                    author:"John Lemon",
+                    date:"2012-10-16T17:57:28.556094Z"
+                },
+                {
+                    rating:4,
+                    comment:"Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                    author:"Paul McVites",
+                    date:"2014-09-05T17:57:28.556094Z"
+                },
+                {
+                    rating:3,
+                    comment:"Eat it, just eat it!",
+                    author:"Michael Jaikishan",
+                    date:"2015-02-13T17:57:28.556094Z"
+                },
+                {
+                    rating:4,
+                    comment:"Ultimate, Reaching for the stars!",
+                    author:"Ringo Starry",
+                    date:"2013-12-02T17:57:28.556094Z"
+                },
+                {
+                    rating:2,
+                    comment:"It's your birthday, we're gonna party!",
+                    author:"25 Cent",
+                    date:"2011-12-02T17:57:28.556094Z"
+                }
+
+            ]
+        };
+
+    }])
+
+    .controller('DishCommentController', ['$scope', function($scope) {
+        // init model
+        $scope.feedback = {
+            date: new Date().toISOString(),
+            rating: 5
+        };
+        // handle submission
+        $scope.submitComment = function () {
+
+            var submittedComment = {
+                rating: parseInt($scope.feedback.rating),
+                comment: $scope.feedback.comments,
+                author: $scope.feedback.name,
+                date: $scope.feedback.date
+            };
+
+            $scope.dish.comments.push(submittedComment);
+
+            //reinit form and model
+            $scope.commentForm.$setPristine();
+            $scope.feedback = {
+                date: new Date().toISOString(),
+                rating: 5
+            };
+        }
     }]);
